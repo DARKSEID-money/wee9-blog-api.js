fc2431c74e65cb7798eb29a466647b764f2e227f
darkseid <eagyei236@gmail.com>
2025-12-01 08:54:32 +0000

    new updated week9 commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const errorhandler = require("./middleware/errorHandler")
 const ArticleRoutes = require("./routes/article.routes");
 
 const app = express()
-const PORT = process.env.PORT
+const PORT =process.env.PORT ||3007
 
 connectDB();
 
@@ -15,12 +15,18 @@ app.use(express.json())
 
 app.use(cors("*"));
 
+app.get("/", (req, res) => {
+    res.send("Api is running...");
+});
+
 
 app.use(RequestLogger);
 
 app.use(ArticleRoutes);
 
 app.use(errorhandler);
+
+
 
 app.listen(PORT, ()=>{
     console.log(`Sever is listening on Port ${PORT}`)
